7acb408fc1b757f0e097d8e029e09fc1b4e0c742
Camille GUILMAIN <caguilmain@hotmail.fr>
2024-11-13 13:04:06 +0100

    partie 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+#["chinese", "italian", "japanese", "french", "belgian"]
+puts "Creation de 5 restos :"
+Restaurant.create(name: "Restaurant 1", address: "Lille", phone_number: "0625252623", category: "chinese")
+Restaurant.create(name: "Restaurant 2", address: "Lille", phone_number: "0625252426", category: "italian")
+Restaurant.create(name: "Restaurant 3", address: "Paris", phone_number: "0758596523", category: "chinese")
+Restaurant.create(name: "Restaurant 4", address: "Marseille", phone_number: "0145236589", category: "french")
+Restaurant.create(name: "Restaurant 5", address: "Lyon", phone_number: "0102050408", category: "belgian")
